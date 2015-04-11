381f164dc3e690f663d95ec47137eb77d9b3a354
fushinoryuu <u0556573@utah.edu>
2015-04-11 14:22:17 -0600

    Changed 1 file: gamemenu.py
    I have made it so the game checks if the user starts to use the keyboard and it will make the mouse cursor disappear.

--- gamemenu.py
+++ gamemenu.py
@@ -5,11 +5,15 @@ import button_class
 
 pygame.init()
 
+white = (255, 255, 255)
+red = (255, 0, 0)
+black = (0, 0, 0)
+
 
 class MenuItem(pygame.font.Font):
     """Class that creates a menu item."""
-    def __init__(self, text, pos=(0,0), font=None, font_size=30,
-                 font_color=(255,255,255)):
+    def __init__(self, text, pos=(0, 0), font=None, font_size=30,
+                 font_color=white):
         pygame.font.Font.__init__(self,font, font_size)
         self.text = text
         self.font_size = font_size
@@ -21,6 +25,7 @@ class MenuItem(pygame.font.Font):
         self.position_x = pos[0]
         self.position_y = pos[1]
         self.position = pos
+        self.is_selected = False
 
     def set_position(self, x, y):
         self.position = (x, y)
@@ -41,7 +46,7 @@ class MenuItem(pygame.font.Font):
 
 class GameMenu():
     """Class that creates a menu."""
-    def __init__(self, screen, items, bg_color=(0, 0, 0), font=None, fon_size=30, font_color=(255, 255, 255)):
+    def __init__(self, screen, items, bg_color=black, font=None, fon_size=30, font_color=white):
 
         self.screen = screen
         self.scr_width = self.screen.get_rect().width
@@ -62,6 +67,46 @@ class GameMenu():
             menu_item.set_position(pos_x, pos_y)
             self.items.append(menu_item)
 
+        self.mouse_is_visible = True
+        self.cur_item = None
+
+    def set_mouse_visibility(self):
+        if self.mouse_is_visible:
+            pygame.mouse.set_visible(True)
+        else:
+            pygame.mouse.set_visible(False)
+
+    def set_item_selection(self, key):
+        for item in self.items:
+            # Return all to neutral.
+            item.set_italic(False)
+            item.set_font_color(white)
+
+        if self.cur_item is None:
+            self.cur_item = 0
+        else:
+            # Find the chose item
+            if key == pygame.K_UP and self.cur_item > 0:
+                self.cur_item -= 1
+            elif key == pygame.K_UP and self.cur_item == 0:
+                self.cur_item = len(self.items) - 1
+            elif key == pygame.K_DOWN and self.cur_item < len(self.items) - 1:
+                self.cur_item += 1
+            elif key == pygame.K_DOWN and self.cur_item == len(self.items) - 1:
+                self.cur_item = 0
+
+        self.items[self.cur_item].set_italic(True)
+        self.items[self.cur_item].set_font_color(red)
+
+    def set_mouse_selection(self, item, mpos):
+        """Marks the MenuItem the mouse cursor hovers on."""
+        if item.is_mouse_selection(mpos):
+            item.set_font_color(red)
+            item.set_italic(True)
+        else:
+            item.set_font_color(white)
+            item.set_italic(False)
+
     def run(self):
         while True:
             self.clock.tick(30)
@@ -74,17 +119,23 @@ class GameMenu():
                     if event.key == K_ESCAPE:
                         pygame.quit()
                         sys.exit()
+                    else:
+                        self.mouse_is_visible = False
+                        self.set_item_selection(event.key)
+
+            if pygame.mouse.get_rel() != (0, 0):
+                self.mouse_is_visible = True
+                self.cur_item = None
+
+            self.set_mouse_visibility()
 
             # Redraw the background.
             self.screen.fill(self.bg_color)
 
             for item in self.items:
-                if item.is_mouse_selection(pygame.mouse.get_pos()):
-                    item.set_font_color((255,0,0))
-                    item.set_italic(True)
-                else:
-                    item.set_font_color((255, 255, 255))
-                    item.set_italic(False)
+                if self.mouse_is_visible:
+                    mpos = pygame.mouse.get_pos()
+                    self.set_mouse_selection(item, mpos)
                 self.screen.blit(item.label, item.position)
 
             pygame.display.flip()
@@ -93,7 +144,7 @@ if __name__ == "__main__":
     # Creating the screen
     screen = pygame.display.set_mode((640, 480), 0, 32)
 
-    menu_items = ('Start', 'Quit')
+    menu_items = ('Start', 'Setting', 'Quit')
 
     pygame.display.set_caption('Game Menu')
     gm = GameMenu(screen, menu_items)
